592ad4650663a79ebd71180b643d9d853dd3707c
nikmikha <nikitam25102123@mail.ru>
2016-12-07 14:52:20 -0800

    Update final.js

--- final.js
+++ final.js
@@ -1,34 +1,34 @@
-$(document).ready(function() {
-	$('.box1').mouseenter(function() {
-	
-		$(this).animate({
-			height: "+=15px"
+	$(document).ready(function() {
+		$('.box1').mouseenter(function() {
+		
+			$(this).animate({
+				height: "+=15px"
+			});
 		});
-	});
 
-	
-	$('.box1').mouseleave(function() {
-		$(this).animate({
-			height: "-=15px"
+		
+		$('.box1').mouseleave(function() {
+			$(this).animate({
+				height: "-=15px"
+			});
 		});
-	});
-	
-	$('.box2').mouseenter(function() {
-	
-		$(this).animate({
-			height: "+=15px"
+		
+		$('.box2').mouseenter(function() {
+		
+			$(this).animate({
+				height: "+=25px"
+			});
 		});
-	});
 
-	
-	$('.box2').mouseleave(function() {
-		$(this).animate({
-			height: "-=15px"
+		
+		$('.box2').mouseleave(function() {
+			$(this).animate({
+				height: "-=25px"
+			});
 		});
-	});
-	
+		
 
-	
-	
- 
- });
+		
+		
+	 
+	 });
